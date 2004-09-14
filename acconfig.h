6017fdcaf908ec439192cb0bd46d5d95091088d9
pandreet <pandreet>
2004-09-14 15:10:19 +0000

    Bug fixed: new definitions in configure.ac

---
+++ acconfig.h
@@ -0,0 +1,17 @@
+/*
+ *  Define when to remove the stringstream use
+ *  from the boost library.
+ */
+#undef BOOST_NO_STRINGSTREAM
+                                                                                
+/*
+ *  Define when we are sure to have the
+ *  right strstream header.
+ */
+#undef HAVE_STRINGSTREAM
+
+/*
+ * Define to know the condorg version
+ */
+#undef CONDORG_VERSION 
+
