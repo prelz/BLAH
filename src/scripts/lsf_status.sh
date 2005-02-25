e0e56efd7a9f0069c3ac2389b402ea5214dd1af7
mezzadri <Massimo.Mezzadri@mi.infn.it>
2005-02-25 14:20:20 +0000

    removed bhist command

--- src/scripts/lsf_status.sh
+++ src/scripts/lsf_status.sh
@@ -2,134 +2,144 @@
 
 #  File:     lsf_status.sh
 #
-#  Author:   David Rebatto
-#  e-mail:   David.Rebatto@mi.infn.it
+#  Author:   David Rebatto, Massimo Mezzadri
+#  e-mail:   David.Rebatto@mi.infn.it, Massimo.Mezzadri@mi.infn.it
 #
 #
 #  Revision history:
+#    20-Mar-2004: Original release
+#    22-Feb-2005: Totally rewritten, bhist command not used anymore
 #
 #  Description:
+#    Return a classad describing the status of a LSF job
 #
 #
 #  Copyright (c) 2004 Istituto Nazionale di Fisica Nucleare (INFN).
 #  All rights reserved.
 #  See http://grid.infn.it/grid/license.html for license details.
 #
-#
-# Initialize env
+
 if  [ -f ~/.bashrc ]; then
- . ~/.bashrc
+	. ~/.bashrc
 fi
 
 if  [ -f ~/.login ]; then
- . ~/.login
+	. ~/.login
 fi
 
 if [ ! -z "$LSF_BIN_PATH" ]; then
-    binpath=${LSF_BIN_PATH}/
+	binpath=${LSF_BIN_PATH}/
 else
-    binpath=/usr/local/lsf/bin/
+	binpath=/usr/local/lsf/bin/
 fi
 
+confpath=${LSF_CONF_PATH:-/etc}
+conffile=$confpath/lsf.conf
+
+lsf_base_path=`cat $conffile|grep LSB_SHAREDIR| awk -F"=" '{ print $2 }'`
+
+lsf_clustername=`${binpath}lsid | grep 'My cluster name is'|awk -F" " '{ print $5 }'`
+logpath=$lsf_base_path/$lsf_clustername/logdir
+
+logeventfile=lsb.events
+
 pars=$*
-requested=`echo $pars | sed 's/^.*\///'`
+requested=`echo $pars | sed -e 's/^.*\///'`
 logfile=`echo $pars | sed 's/\/.*//'`
 
-# bhist output formatted as a classad
+typeset -i lognum=`echo $logfile | sed 's/^.*\.//'`
 
-typeset -i lognum=`echo $logfile | sed 's/^.*\.//'`+1
+# Create the list of log files to scan
+# in the right order (older first)
+if [ $lognum -gt 1 ]; then
+	for i in `seq $lognum -1 1`; do
+		logs="$logs $logpath/$logeventfile.$i"
+	done
+elif [ $lognum -eq 1 ]; then
+	logs="$logpath/$logeventfile.1"
+fi
+logs="$logs $logpath/$logeventfile"
 
-result=`${binpath}bhist -n $lognum -l $requested | awk '
-/^[^ ]/ {
-    print current_value 
-    current_value = $0
-}
-/^[ ]+/ {
-    current_value = current_value substr($0, 22)
-}
-END{
- print current_value 
-}
-'|awk '
+#/* job states */
+#define JOB_STAT_NULL         0x00
+#define JOB_STAT_PEND         0x01
+#define JOB_STAT_PSUSP        0x02
+#define JOB_STAT_RUN          0x04
+#define JOB_STAT_SSUSP        0x08
+#define JOB_STAT_USUSP        0x10
+#define JOB_STAT_EXIT         0x20
+#define JOB_STAT_DONE         0x40
+#define JOB_STAT_PDONE        (0x80)  /* Post job process done successfully */
+#define JOB_STAT_PERR         (0x100) /* Post job process has error */
+#define JOB_STAT_WAIT         (0x200) /* Chunk job waiting its turn to exec */
+#define JOB_STAT_UNKWN        0x10000
+
+result=`awk -v jobId=$requested '
 BEGIN {
-    current_job = ""
-    current_out = ""
-    current_err = ""
-    current_time = ""
-    current_exit = ""
-    current_status = "1"
+	rex_queued   = "\"JOB_NEW\" \"[0-9\.]+\" [0-9]+ " jobId
+	rex_running  = "\"JOB_START\" \"[0-9\.]+\" [0-9]+ " jobId
+	rex_deleted  = "\"JOB_SIGNAL\" \"[0-9\.]+\" [0-9]+ " jobId "[0-9]+ [0-9]+ \"KILL\""
+	rex_done     = "\"JOB_STATUS\" \"[0-9\.]+\" [0-9]+ " jobId " 192 "
+	rex_finished = "\"JOB_STATUS\" \"[0-9\.]+\" [0-9]+ " jobId " 32 "
+	rex_hold     = "\"JOB_STATUS\" \"[0-9\.]+\" [0-9]+ " jobId " 16 "
+	rex_resume   = "\"JOB_STATUS\" \"[0-9\.]+\" [0-9]+ " jobId " 4 "
+        
+	jobstatus = 0
+	
+	print "["
+	print "BatchjobId = \"" jobId "\";"
 }
 
-/^Job/ {
-    current_job = substr($0, index($0, "Job <") + 5, index($0, ">") - index($0, "Job <") - 5)
-    print "[\nBatchjobId = \"" current_job "\";"
+$0 ~ rex_queued {
+	jobstatus = 1
 }
 
-/Output File/  {
-    current_out = substr($0, index($0, "Output File <") + 13, index($0, ">, Error File") - index($0, "Output File <") - 13)
-    print "Out = \"" current_out "\";"
+$0 ~ rex_running {
+	jobstatus = 2
 }
 
-/Error File/ {
-    current_err = substr($0, index($0, "Error File <") + 12, index($0, ">, Notify") - index($0, "Error File <") - 12)
-    if(current_err) {
-     print "Err = \"" current_err "\";"
-    }else{
-     current_err = substr($0, index($0, "Error File <") + 12, index($0, ">, Copy") - index($0, "Error File <") - 12)
-     print "Err = \"" current_err "\";"
-    }
+$0 ~ rex_deleted {
+	jobstatus = 3
+	exit
 }
 
-/Start/ {
-    current_time = substr($0, 0, index($0, "Start") - 3)
-    print "Started = \"" current_time "\";"
-    current_status = "2"
+$0 ~ rex_done {
+	jobstatus = 4
+	exitcode = 0
+	exit
 }
 
-/Suspended by the user/ {
-  current_status = "5"
+$0 ~ rex_finished {
+	jobstatus = 4
+	exitcode = $(NF-1)
+	exit
 }
 
-/Done successfully/ {
-    current_exit = 0
-    print "ExitCode = " current_exit ";"
-    current_status = "4"
+$0 ~ rex_hold {
+	jobstatus = 5
 }
 
-/Exited with exit code/ {
-    current_exit = substr($0, index($0, "Exited with exit code") + 22, index($0, ".") - index($0, "Exited with exit code") - 22)
-    print "ExitCode = " current_exit ";"
-    if(current_status == 3){
-     current_status = "3"
-    }else{
-     current_status = "4"
-    }
-}
-
-/Signal <KILL>/ {
-  current_status = "3"
-}
-
-/No matching job found/ {
-  current_status = "-1"
+$0 ~ rex_resume {
+	jobstatus = 2
 }
 
 END {
-    if (current_status != -1){ 
-    print "JobStatus = " current_status ";"
-    print "]"
-    }
+	if (jobstatus == 0) { exit 1 }
+	print "JobStatus = " jobstatus ";"
+	if (jobstatus == 4) {
+		print "ExitCode = " exitcode ";"
+	}
+	print "]"
 }
-'
-`
-# If bhist doesn't know about the job,
-if [ "x$result" == "x" ]; then
- echo "Error: cannot retrieve job status"
- retcode=1
-else 
- retcode=0
- echo $result
+' $logs`
+
+if [ "$?" == "0" ] ; then
+        echo $result
+        retcode=0
+else
+        echo "ERROR: Job not found"
+        retcode=1
 fi
 
-echo \*\*\* END \*\*\*
 exit $retcode
+
