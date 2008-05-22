51c899cb4abb957646642d01d4d534d6ec70d570
mezzadri <Massimo.Mezzadri@mi.infn.it>
2008-05-22 09:23:56 +0000

    suspended state is now considered when we run final state query

--- src/BUpdaterPBS.c
+++ src/BUpdaterPBS.c
@@ -200,13 +200,13 @@ int main(int argc, char *argv[]){
 		while ((en = job_registry_get_next(rha, fd)) != NULL)
 		{
 
-			/* Assign Status=4 and ExitStatus=-1 to all entries that after alldone_interval are still not in a final state(3 or 4) */
-			if((now-en->mdate>alldone_interval) && en->status!=3 && en->status!=4)
+			/* Assign Status=4 and ExitStatus=-1 to all entries that after alldone_interval are still not in a final state(3 or 4) or in suspended state (5) */
+			if((now-en->mdate>alldone_interval) && en->status!=3 && en->status!=4 && en->status!=5)
 			{
 				AssignFinalState(en->batch_id);	
 			}
 			
-			if((now-en->mdate>finalstate_query_interval) && en->status!=3 && en->status!=4)
+			if((now-en->mdate>finalstate_query_interval) && en->status!=3 && en->status!=4 && en->status!=5)
 			{
 				strcat(final_string,en->batch_id);
 				strcat(final_string,":");
