0b0feacab5610cccda7156a2099f81518f78f84a
mezzadri <Massimo.Mezzadri@mi.infn.it>
2008-05-27 13:33:57 +0000

    readded -u all from bjobs and bhist call

--- src/BUpdaterLSF.c
+++ src/BUpdaterLSF.c
@@ -311,7 +311,7 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 		sysfatal("can't malloc command_string %r");
 	}
 	
-	sprintf(command_string,"%s/bjobs -a -l",lsf_binpath);
+	sprintf(command_string,"%s/bjobs -u all -a -l",lsf_binpath);
 	file_output = popen(command_string,"r");
 
 	if (file_output != NULL){
@@ -474,7 +474,7 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 		sysfatal("can't malloc command_string %r");
 	}
 
-	sprintf(command_string,"%s/bhist -a -l -n %d",lsf_binpath,bhist_logs_to_read);
+	sprintf(command_string,"%s/bhist -u all -a -l -n %d",lsf_binpath,bhist_logs_to_read);
 	file_output = popen(command_string,"r");
 
         if (file_output != NULL){
