db9106d0b6f79636cd258e701165634a2f50a23e
prelz <Francesco.Prelz@mi.infn.it>
2008-07-04 12:13:56 +0000

    Removed spurious call to iepoch2str.

--- src/BUpdaterLSF.c
+++ src/BUpdaterLSF.c
@@ -523,7 +523,7 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 	if(en.status!=UNDEFINED){	
 		if(debug>1){
 			dgbtimestamp=iepoch2str(time(0));
-			fprintf(debuglogfile, "%s %s: registry update in FinalStateQuery for: jobid=%s wn=%s status=%d\n",iepoch2str(dgbtimestamp),argv0,en.batch_id,en.wn_addr,en.status);
+			fprintf(debuglogfile, "%s %s: registry update in FinalStateQuery for: jobid=%s wn=%s status=%d\n",dgbtimestamp,argv0,en.batch_id,en.wn_addr,en.status);
 			fflush(debuglogfile);
 			free(dgbtimestamp);
 		}
