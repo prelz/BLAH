cfb857134f63745304985c4b482e09ff0da201cc
mezzadri <Massimo.Mezzadri@mi.infn.it>
2009-01-08 11:22:24 +0000

    fixed a problem with canceled jobs

--- src/BUpdaterLSF.c
+++ src/BUpdaterLSF.c
@@ -318,7 +318,7 @@ IntStateQuery()
 	fp = popen(command_string,"r");
 
 	en.status=UNDEFINED;
-	en.wn_addr[0]='\0';
+	JOB_REGISTRY_ASSIGN_ENTRY(en.wn_addr,"N/A");
 	bupdater_free_active_jobs(&bact);
 	
 	if(fp!=NULL){
@@ -481,6 +481,12 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 				
 			if(line && strstr(line,"Job <")){	
 				if(en.status!=UNDEFINED){	
+					if(debug>1){
+						dgbtimestamp=iepoch2str(time(0));
+						fprintf(debuglogfile, "%s %s: registry update in FinalStateQuery for: jobid=%s wn=%s status=%d\n",dgbtimestamp,argv0,en.batch_id,en.wn_addr,en.status);
+						fflush(debuglogfile);
+						free(dgbtimestamp);
+					}
                         		if ((ret=job_registry_update(rha, &en)) < 0){
 						if(ret != JOB_REGISTRY_NOT_FOUND){
                 	                		fprintf(stderr,"Append of record returns %d: ",ret);
@@ -494,7 +500,20 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 				JOB_REGISTRY_ASSIGN_ENTRY(en.batch_id,batch_str);
 				free(batch_str);
 				freetoken(&token,maxtok_t);
-			}else if(line && strstr(line," Exited with exit code")){	
+			}else if(line && strstr(line," Signal <KILL>")){	
+				maxtok_t = strtoken(line, ' ', &token);
+                        	if((timestamp=malloc(strlen(token[0]) + strlen(token[1]) + strlen(token[2]) + strlen(token[3]) + 4)) == 0){
+					sysfatal("can't malloc timestamp in FinalStateQuery: %r");
+				}
+				sprintf(timestamp,"%s %s %s %s",token[0],token[1],token[2],token[3]);
+				timestamp[strlen(timestamp)-1]='\0';
+				tmstampepoch=str2epoch(timestamp,"W");
+				en.udate=tmstampepoch;
+				en.status=REMOVED;
+				free(timestamp);
+				JOB_REGISTRY_ASSIGN_ENTRY(en.exitreason,"\0");
+				freetoken(&token,maxtok_t);
+			}else if(line && strstr(line," Exited with exit code") && en.status != REMOVED){	
 				maxtok_t = strtoken(line, ' ', &token);
                         	if((timestamp=malloc(strlen(token[0]) + strlen(token[1]) + strlen(token[2]) + strlen(token[3]) + 4)) == 0){
 					sysfatal("can't malloc timestamp in FinalStateQuery: %r");
@@ -510,7 +529,7 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 				free(ex_str);
 				JOB_REGISTRY_ASSIGN_ENTRY(en.exitreason,"\0");
 				freetoken(&token,maxtok_t);
-			}else if(line && strstr(line," Done successfully")){	
+			}else if(line && strstr(line," Done successfully") && en.status != REMOVED){	
 				maxtok_t = strtoken(line, ' ', &token);
                         	if((timestamp=malloc(strlen(token[0]) + strlen(token[1]) + strlen(token[2]) + strlen(token[3]) + 4)) == 0){
 					sysfatal("can't malloc timestamp in FinalStateQuery: %r");
@@ -522,19 +541,6 @@ exitcode (=0 if Done successfully) or (from Exited with exit code 2)
 				en.status=COMPLETED;
 				free(timestamp);
 				en.exitcode=0;
-				JOB_REGISTRY_ASSIGN_ENTRY(en.exitreason,"\0");
-				freetoken(&token,maxtok_t);
-			}else if(line && strstr(line," Signal <KILL>")){	
-				maxtok_t = strtoken(line, ' ', &token);
-                        	if((timestamp=malloc(strlen(token[0]) + strlen(token[1]) + strlen(token[2]) + strlen(token[3]) + 4)) == 0){
-					sysfatal("can't malloc timestamp in FinalStateQuery: %r");
-				}
-				sprintf(timestamp,"%s %s %s %s",token[0],token[1],token[2],token[3]);
-				timestamp[strlen(timestamp)-1]='\0';
-				tmstampepoch=str2epoch(timestamp,"W");
-				en.udate=tmstampepoch;
-				en.status=REMOVED;
-				free(timestamp);
 				JOB_REGISTRY_ASSIGN_ENTRY(en.exitreason,"\0");
 				freetoken(&token,maxtok_t);
 			}
