c733580802a91a5159a8b2881f7d98f402d0757e
prelz <Francesco.Prelz@mi.infn.it>
2009-06-15 16:11:33 +0000

    Added extra features. Selection by job status. Format string check. Job list number logical argument.

--- src/blah_job_registry_scan_by_subject.c
+++ src/blah_job_registry_scan_by_subject.c
@@ -96,7 +96,80 @@ undo_escapes(char *string)
   if (rp != wp) *wp = '\000';
 }
 
-#define USAGE_STRING "ERROR Usage: %s (<-s (proxy subject)>|<-h (proxy subject hash)> \"Optional arg1 format\" arg1 \"Optional arg2 format\" arg2, etc.\n"
+typedef enum format_type_e
+{
+  NO_FORMAT,
+  FORMAT_INT,
+  FORMAT_STRING,
+  FORMAT_FLOAT,
+  FORMAT_UNKNOWN
+} format_type;
+
+format_type
+get_format_type(char *fmt, int which, int *totfmts)
+{
+  char *pc;
+  char *sp;
+  char *start, *end;
+  int nf = 0;
+  format_type result = NO_FORMAT;
+
+  start = fmt;
+  for(start = fmt; ((pc = strchr(start, '%')) != NULL); start = end+1)
+   {
+    end = pc;
+    if (*(pc+1) == '%') continue;
+
+    for (end = pc+1; *end != '\000'; end++)
+     {
+      /* Skip over format qualifiers */
+      if (*end <  '0' || ( *end >  '9' && *end != '#' &&
+          *end != ' ' && *end != '-' && *end != '+' &&
+          *end != '\'' && *end != '.' && *end != 'l' &&
+          *end != 'L' && *end != 'h' && *end != 'q' &&
+          *end != 'j' && *end != 'q' && *end != 'z' &&
+          *end != 't' ) ) break;
+     }
+
+    if (*end == '\000') break;
+
+    nf++;
+    if (nf == which)
+     {
+      switch (*end)
+       {
+        case 's':
+          result = FORMAT_STRING;
+          break;
+        case 'x':
+        case 'X':
+        case 'o':
+        case 'd':
+        case 'i':
+        case 'u':
+          result = FORMAT_INT;
+          break;
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'f':
+        case 'F':
+        case 'g':
+        case 'G':
+          result = FORMAT_FLOAT;
+          break;
+        default:
+          result = FORMAT_UNKNOWN;
+       }
+      if (totfmts == NULL) break;
+     }
+   }
+  if (totfmts != NULL) *totfmts = nf;
+  return result;
+}
+
+#define USAGE_STRING "ERROR Usage: %s (<-s (proxy subject)>|<-h (proxy subject hash>) [-j job status] \"Optional arg1 format\" arg1 \"Optional arg2 format\" arg2, etc.\n"
 int
 main(int argc, char *argv[])
 {
@@ -112,11 +185,17 @@ main(int argc, char *argv[])
   job_registry_handle *rha;
   FILE *fd;
   char *arg = "";
+  int iarg;
   char *looked_up_subject = NULL;
   char *lookup_subject = NULL;
-  char *lookup_hash;
-  int format_args = -1;
+  char *lookup_hash = NULL;
+  int cur_arg;
+  int format_args;
+  int select_by_job_status = -1;
   int ifr;
+  int njobs = 0;
+  format_type first_fmt;
+  int nfmts;
  
   if (argc < 2)
    {
@@ -124,51 +203,72 @@ main(int argc, char *argv[])
     return 1;
    }
 
-  /* Look up for command line switches */
-  if (argv[1][0] != '-')
-   {
-    fprintf(stderr,USAGE_STRING,argv[0]);
-    return 1;
-   }
+  cur_arg = 1;
 
-  if (strlen(argv[1]) > 2)
+  while (argv[cur_arg][0] == '-')
    {
-    arg = argv[1] + 2;
-    if (argc > 2)
+    format_args = -1;
+    /* Look up for command line switches */
+    if (strlen(argv[cur_arg]) > 2)
      {
-      format_args = 2;
+      arg = argv[cur_arg] + 2;
+      if (argc > (cur_arg+1))
+       {
+        format_args = cur_arg+1;
+       }
      }
-   }
-  else if (argc > 2)
-   {
-    arg = argv[2]; 
-    if (argc > 3)
+    else if (argc > (cur_arg+1))
      {
-      format_args = 3;
+      arg = argv[cur_arg+1]; 
+       if (argc > (cur_arg+2))
+        {
+         format_args = cur_arg+2;
+        }
      }
-   }
 
-  if (strlen(arg) <= 0)
-   {
-    fprintf(stderr,USAGE_STRING,argv[0]);
-    return 1;
-   }
-
-  switch (argv[1][1])
-   {
-    case 'h':
-      lookup_hash = arg;
-      break;
-    case 's':
-      job_registry_compute_subject_hash(&hen, arg);
-      lookup_subject = arg;
-      lookup_hash = hen.subject_hash;
-      break;
-    default:
+    if (strlen(arg) <= 0)
+     {
       fprintf(stderr,USAGE_STRING,argv[0]);
       return 1;
+     }
+
+    switch (argv[cur_arg][1])
+     {
+      case 'h':
+        if (lookup_hash != NULL)
+         {
+          fprintf(stderr,USAGE_STRING,argv[0]);
+          return 1;
+         }
+        lookup_hash = arg;
+        break;
+      case 's':
+        if (lookup_hash != NULL)
+         {
+          fprintf(stderr,USAGE_STRING,argv[0]);
+          return 1;
+         }
+        job_registry_compute_subject_hash(&hen, arg);
+        lookup_subject = arg;
+        lookup_hash = hen.subject_hash;
+        break;
+      case 'j':
+        select_by_job_status = atoi(arg);
+        break;
+      default:
+        fprintf(stderr,USAGE_STRING,argv[0]);
+        return 1;
+     }
+    if ((format_args > 0) && (format_args < argc)) cur_arg = format_args;
+    else break;
    }
     
+  if (lookup_hash == NULL)
+   {
+    fprintf(stderr,USAGE_STRING,argv[0]);
+    return 1;
+   }
+
   cha = config_read(NULL); /* Read config from default locations. */
   if (cha != NULL)
    {
@@ -254,6 +354,12 @@ main(int argc, char *argv[])
 
   while ((ren = job_registry_get_next_hash_match(rha, fd, lookup_hash)) != NULL)
    {
+    /* Is the current entry in the requested job status ? */
+    if ((select_by_job_status > 0) && (ren->status != select_by_job_status))
+      continue;
+
+    njobs++;
+
     cad = job_registry_entry_as_classad(rha, ren);
     if (cad != NULL)
      {
@@ -272,16 +378,28 @@ main(int argc, char *argv[])
          }
         for (ifr = format_args; ifr < argc; ifr+=2)
          {
+          first_fmt = get_format_type(argv[ifr], 1, &nfmts);
+          if (nfmts == 0) { ifr--; continue; }
+          if (nfmts > 1) { continue; } /* Deal with one conversion at a time */
           if ((ifr+1) >= argc)
            {
             printf(argv[ifr]);
            }
           else
            {
-            if (classad_get_dstring_attribute(pcad, argv[ifr+1], &arg) == C_CLASSAD_NO_ERROR)
+            if (strncmp(argv[ifr+1], "Njob", 4) == 0)
              {
-              printf(argv[ifr],arg);
+              /* Print the order number of the job */
+              if (first_fmt == FORMAT_INT) printf(argv[ifr],njobs);
+             }
+            else if (classad_get_dstring_attribute(pcad, argv[ifr+1], &arg) == C_CLASSAD_NO_ERROR)
+             {
+              if (first_fmt == FORMAT_STRING) printf(argv[ifr],arg);
               free(arg);
+             }
+            else if (classad_get_int_attribute(pcad, argv[ifr+1], &iarg) == C_CLASSAD_NO_ERROR)
+             {
+              if (first_fmt == FORMAT_INT) printf(argv[ifr],iarg);
              }
            }
          }
