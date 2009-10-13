a0daf4527770c2a11cb5c9b4d2e9e94365287159
drebatto <David.Rebatto@mi.infn.it>
2009-10-13 09:24:01 +0000

    Relaxing the error condition on signals received by the poll() function call. This modification was lost when mapped_exec.c replaced mtsafe_popen.c

--- src/mapped_exec.c
+++ src/mapped_exec.c
@@ -466,7 +466,7 @@ execute_cmd(exec_cmd_t *cmd)
 					if (successful_read == 0)
 					{
 						/* add a message to stderr if the signal is not POLLHUP */
-						if (!((pipe_poll[0].revents & POLLHUP) && (pipe_poll[1].revents & POLLHUP)))
+						if (!((pipe_poll[0].revents & POLLHUP) || (pipe_poll[1].revents & POLLHUP)))
 						{
 							new_cmd_error = make_message(killed_for_poll_signal, cmd->error, pipe_poll[0].revents, pipe_poll[1].revents);
 							if (new_cmd_error != NULL)
