89d1595585a7ac35f2bf490b08eb411033e08abd
mezzadri <Massimo.Mezzadri@mi.infn.it>
2010-03-17 11:03:12 +0000

    added license

--- src/blah_job_registry_scan_by_subject.c
+++ src/blah_job_registry_scan_by_subject.c
@@ -1,7 +1,7 @@
 /*
  *  File :     blah_job_registry_scan_by_subject.c
  *
- *  Author :   Francesco Prelz ($Author: fprelz $)
+ *  Author :   Francesco Prelz ($Author: mezzadri $)
  *  e-mail :   "francesco.prelz@mi.infn.it"
  *
  *  Revision history :
@@ -12,9 +12,22 @@
  *   that match a given proxy subject, and print them in variable
  *   formats.
  *
- *  Copyright (c) 2007 Istituto Nazionale di Fisica Nucleare (INFN).
- *   All rights reserved.
- *   See http://grid.infn.it/grid/license.html for license details.
+ *  Copyright: (c) Members of the EGEE Collaboration. 2007-2010. 
+ *
+ *    See http://www.eu-egee.org/partners/ for details on the copyright
+ *    holders.  
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License"); 
+ *    you may not use this file except in compliance with the License. 
+ *    You may obtain a copy of the License at 
+ *  
+ *        http://www.apache.org/licenses/LICENSE-2.0 
+ *  
+ *    Unless required by applicable law or agreed to in writing, software 
+ *    distributed under the License is distributed on an "AS IS" BASIS, 
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *    See the License for the specific language governing permissions and 
+ *    limitations under the License.
  */
 
 #include <stdio.h>
