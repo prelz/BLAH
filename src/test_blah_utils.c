d3242c784b4b63b2230bc544d79c5300b70bf5b0
David Rebatto <david.rebatto@mi.infn.it>
2016-04-07 16:59:11 +0200

    Added header comment

--- src/test_blah_utils.c
+++ src/test_blah_utils.c
@@ -1,5 +1,36 @@
+/*
+ *  File :     test_blah_utils.c
+ *
+ *
+ *  Author :   David Rebatto
+ *  e-mail :   "david.rebatto@mi.infn.it"
+ *
+ *  Revision history :
+ *  5-Apr-2016 Original release
+ *
+ *  Description:
+ *   Collect contents statistics from a test job registry.
+ *
+ *  Copyright (c) Members of the EGEE Collaboration. 2007-2010. 
+ *
+ *    See http://www.eu-egee.org/partners/ for details on the copyright
+ *    holders.  
+ *  
+ *    Licensed under the Apache License, Version 2.0 (the "License"); 
+ *    you may not use this file except in compliance with the License. 
+ *    You may obtain a copy of the License at 
+ *  
+ *        http://www.apache.org/licenses/LICENSE-2.0 
+ *  
+ *    Unless required by applicable law or agreed to in writing, software 
+ *    distributed under the License is distributed on an "AS IS" BASIS, 
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *    See the License for the specific language governing permissions and 
+ *    limitations under the License.
+ */
+
+
 #include <stdio.h>
-#include <wordexp.h>
 #include <errno.h>
 #include <string.h>
 #include <assert.h>
