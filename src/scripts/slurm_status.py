c2d4b6adc2a3b70d3a2835d49aff68e4af633940
Brian Lin <brianhlin@gmail.com>
2017-02-15 18:37:53 -0600

    Fail gracefully when parsing unexpected sacct output (SOFTWARE-2604)

--- src/scripts/slurm_status.py
+++ src/scripts/slurm_status.py
@@ -278,9 +278,8 @@ def which(program):
     return None
 
 def convert_cpu_to_seconds(cpu_string):
-    import re
-    h,m,s = re.split(':',cpu_string)
-    return int(h) * 3600 + int(m) * 60 + int(s)
+    hrs, mins, secs = re.split(':', cpu_string)
+    return int(hrs) * 3600 + int(mins) * 60 + int(secs)
 
 def get_finished_job_stats(jobid):
     """
@@ -308,20 +307,22 @@ def get_finished_job_stats(jobid):
     except Exception, e:
         log("Unable to read in CSV output from sacct: %s" % str(e))
         return return_dict
-           
+
+    sacct_parser = {'RemoteUserCpu': lambda orig, results: orig + \
+                    convert_cpu_to_seconds(results["AveCPU"]) * int(results["AllocCPUS"]),
+                    'ImageSize': lambda orig, results: orig + int(results["MaxRSS"].replace('K', '')),
+                    'ExitCode': lambda orig, results: int(results["ExitCode"].split(":")[0])}
     # Slurm can return more than 1 row, for some odd reason.
     # so sum up relevant values
     for row in reader:
-        if row["AveCPU"] is not "":
-            return_dict['RemoteUserCpu'] += convert_cpu_to_seconds(row["AveCPU"]) * int(row["AllocCPUS"])
-        if row["MaxRSS"] is not "":
-            # Remove the trailing 'K'
-            return_dict["ImageSize"] += int(row["MaxRSS"].replace('K', ''))
-        if row["ExitCode"] is not "":
-            return_dict["ExitCode"] = int(row["ExitCode"].split(":")[0])
+        for attr, func in sacct_parser.items():
+            try:
+                return_dict[attr] = func(return_dict[attr], row)
+            except (ValueError, KeyError), exc:
+                log("Could not parse %s for Jobid %s: %s" % (attr, jobid, exc))
 
     return return_dict
-    
+
 
 _slurm_location_cache = None
 def get_slurm_location(program):
