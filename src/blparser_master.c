7148c91b0df21e99c50d12c35c68a676ecd520b7
Carl Edquist <edquist@cs.wisc.edu>
2018-06-06 12:27:08 -0500

    failures after fork() should _exit()
    return is especially bad since it leaves both the parent and child alive

--- src/blparser_master.c
+++ src/blparser_master.c
@@ -147,15 +147,15 @@ check_on_children_args(const struct blah_managed_child *children, const int coun
 				if((j = wordexp(children[i].exefile, &args, WRDE_NOCMD)))
 				{
 					fprintf(stderr,"wordexp: unable to parse the command line \"%s\" (error %d)\n", children[i].exefile, j);
-                			return;
-        			}
+					_exit(1);
+				}
 				/* Child process. Exec exe file. */
 				if (execv(args.we_wordv[0], args.we_wordv) < 0)
 				{
 					fprintf(stderr,"Cannot exec %s: %s\n",
 						children[i].exefile,
 						strerror(errno));
-					exit(1);
+					_exit(1);
 				}
 				/* Free the wordexp'd args */
  				wordfree(&args);
